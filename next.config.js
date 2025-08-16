ee01849229171df2c995f6ac219484ceb2cb0248
Leo <96000138+Ember0810@users.noreply.github.com>
2025-08-16 11:49:08 +0800

    Update next.config.js
    reset default

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-const isProd = process.env.NODE_ENV === 'production';
-
-// const nextConfig = {
-//   output: 'export',
-//   basePath: isProd ? '/jjao-web-official' : '',
-//   assetPrefix: isProd ? '/jjao-web-official' : '',
-// };
-
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true, 
-  },
-  basePath: '/jjao-web-official',
-  assetPrefix: '/jjao-web-official'
-};
+  reactStrictMode: true,
+}
 
 module.exports = nextConfig;
